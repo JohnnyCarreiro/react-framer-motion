60962acdb0e6932a7e9d3eea9087a4ad263e6336
Johnny <jcarreiropublicidade@gmail.com>
2021-05-03 22:56:23 -0300

    feat: Update main libs, react, next and react-dom

--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.div `
   min-height: 100vh;
+  width: 100%;
+  height: 100%;
   padding: 0 0.5rem;
+  margin: 0 auto;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
